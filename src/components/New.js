3804e615dbadb66937cae78eb8035a926192e275
Ricardo Diaz <Ricardo.Diaz@endava.com>
2019-12-02 15:11:13 -0500

    showing news

---
+++ src/components/New.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const New = ({ noticie }) => {
+
+    const { urlToImage, url, title, description, source } = noticie;
+    return (
+
+        <div className="col s12 m6 l4">
+            <div className="card">
+                <div className="card-image">
+                    <img src={urlToImage} alt={title} />
+                </div>
+                <div className="card-content">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+                <div className = "card-action">
+                    <a href = {url} target = "_blank"  rel="noopener noreferrer" className ="waves-effect waves-light btn" >
+                        Ver Noticia
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default New;
